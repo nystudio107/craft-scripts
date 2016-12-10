6dbeddcdb6c9223a3a688ceb9d91ac62daedd0c4
Andrew Welch <andrew@megalomaniac.com>
2016-12-10 10:22:31 -0500

    [Improved] Removed echo of TMP_DB_PATH

--- scripts/pull_db.sh
+++ scripts/pull_db.sh
@@ -12,7 +12,6 @@ source ".env.sh"
 TMP_DB_PATH="/tmp/"$REMOTE_DB_NAME"-db-dump-"$(date '+%Y%m%d')".sql"
 BACKUP_DB_PATH="/tmp/"$LOCAL_DB_NAME"-db-backup-"$(date '+%Y%m%d')".sql"
 
-echo $TMP_DB_PATH;
 ssh $REMOTE_SSH_LOGIN "mysqldump -u '$REMOTE_DB_USER' -p'$REMOTE_DB_PASSWORD' '$REMOTE_DB_NAME' > $TMP_DB_PATH"
 scp -- $REMOTE_SSH_LOGIN:"$TMP_DB_PATH" "$TMP_DB_PATH"
 $LOCAL_MYSQLDUMP_CMD -u "$LOCAL_DB_USER" -p"$LOCAL_DB_PASSWORD" "$LOCAL_DB_NAME" > "$BACKUP_DB_PATH"
