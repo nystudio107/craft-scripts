4a924ba4a896561bc751bb84560d2d97ac2509d7
Dennis Frank <mail@dennisfrank.info>
2017-08-25 01:39:57 +0200

    Add symlink support to rsync commands
    rsync -L: transform symlink into referent file/dir

--- scripts/pull_assets.sh
+++ scripts/pull_assets.sh
@@ -40,7 +40,7 @@ fi
 # Pull down the asset dir files via rsync
 for DIR in "${LOCAL_ASSETS_DIRS[@]}"
 do
-    rsync -F -a -z -e "ssh -p ${REMOTE_SSH_PORT}" "${REMOTE_SSH_LOGIN}:${REMOTE_ASSETS_PATH}${DIR}" "${LOCAL_ASSETS_PATH}" --delete-after --progress
+    rsync -F -L -a -z -e "ssh -p ${REMOTE_SSH_PORT}" "${REMOTE_SSH_LOGIN}:${REMOTE_ASSETS_PATH}${DIR}" "${LOCAL_ASSETS_PATH}" --delete-after --progress
     echo "*** Synced assets from ${REMOTE_ASSETS_PATH}${DIR}"
 done
 
@@ -53,7 +53,7 @@ fi
 # Pull down the Craft-specific dir files via rsync
 for DIR in "${LOCAL_CRAFT_FILE_DIRS[@]}"
 do
-    rsync -F -a -z -e "ssh -p ${REMOTE_SSH_PORT}" "${REMOTE_SSH_LOGIN}:${REMOTE_CRAFT_FILES_PATH}${DIR}" "${LOCAL_CRAFT_FILES_PATH}" --delete-after --progress
+    rsync -F -L -a -z -e "ssh -p ${REMOTE_SSH_PORT}" "${REMOTE_SSH_LOGIN}:${REMOTE_CRAFT_FILES_PATH}${DIR}" "${LOCAL_CRAFT_FILES_PATH}" --delete-after --progress
     echo "*** Synced assets from ${REMOTE_CRAFT_FILES_PATH}${DIR}"
 done
 
